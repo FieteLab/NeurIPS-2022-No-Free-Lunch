f6a7ecf759741e2cb1a8e0c5e9aa2fdd753356c8
RylanSchaeffer <rylanschaeffer@gmail.com>
2022-03-26 12:47:48 -0700

    Added more plots

--- mec_hpc_investigations/models/plot.py
+++ mec_hpc_investigations/models/plot.py
@@ -8,18 +8,21 @@ plt.rcParams["font.size"] = 20  # was previously 22
 sns.set_style("whitegrid")
 
 
-def plot_percent_pos_decoding_err_vs_max_grid_score_by_run_group(
+def plot_pos_decoding_err_vs_max_grid_score_by_run_group(
         runs_performance_df: pd.DataFrame,
         plot_dir: str):
-
-    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8))
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8),
+                             sharey=True, sharex=True)
+    ymin = runs_performance_df['pos_decoding_err'].min()
+    ymax = runs_performance_df['pos_decoding_err'].max()
     ax = axes[0]
     sns.scatterplot(y="pos_decoding_err",
                     x='max_grid_score_d=60_n=256',
                     data=runs_performance_df,
                     hue='run_group',
                     ax=ax,
-                    s=2)
+                    s=5)
+    ax.set_ylim(ymin, ymax)
     ax.set_ylabel('Pos Decoding Err')
     ax.set_xlabel(r'Max Grid Score ($60^{\circ}$)')
 
@@ -29,49 +32,95 @@ def plot_percent_pos_decoding_err_vs_max_grid_score_by_run_group(
                     data=runs_performance_df,
                     hue='run_group',
                     ax=ax,
-                    s=2)
+                    s=10)
+    ax.set_ylim(ymin, ymax)
+    ax.set_ylabel(None)  # Share Y-Label with subplot to left.
     ax.set_xlabel(r'Max Grid Score ($60^{\circ}$)')
-    ax.set_title(r'$90^{\circ}$')
+    # ax.set_title(r'$90^{\circ}$')
 
     plt.savefig(os.path.join(plot_dir,
-                             f'percent_pos_decoding_err_vs_max_grid_score.pdf'),
+                             f'pos_decoding_err_vs_max_grid_score.pdf'),
                 bbox_inches='tight',
                 dpi=300)
     # plt.show()
     plt.close()
 
 
-def plot_percent_pos_decoding_err_vs_run_group(
+def plot_percent_have_grid_cells_vs_run_group_given_low_pos_decoding_err(
+        runs_performance_df: pd.DataFrame,
+        plot_dir: str,
+        low_pos_decoding_err_threshold: float = 5.,
+        grid_score_d60_threshold: float = 1.0,
+        grid_score_d90_threshold: float = 1.5):
+    runs_performance_df[f'pos_decoding_err_below_{low_pos_decoding_err_threshold}'] = \
+        runs_performance_df['pos_decoding_err'] < low_pos_decoding_err_threshold
+
+    runs_performance_df[f'has_grid_d60'] \
+        = runs_performance_df['max_grid_score_d=60_n=256'] > grid_score_d60_threshold
+
+    runs_performance_df[f'has_grid_d90'] \
+        = runs_performance_df['max_grid_score_d=90_n=256'] > grid_score_d90_threshold
+
+    fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(12, 8),
+                             sharey=True, sharex=True)
+
+    ax = axes[0]
+    sns.barplot(y="has_grid_d60",
+                x='run_group',
+                data=runs_performance_df,
+                ax=ax)
+    ax.set_ylim(0., 1.)
+    ax.set_ylabel(f'% Runs : Max Grid Score > {grid_score_d60_threshold} | Pos Err < {low_pos_decoding_err_threshold}')
+    ax.set_xlabel('Group')
+
+    ax = axes[1]
+    sns.barplot(y="has_grid_d90",
+                x='run_group',
+                data=runs_performance_df,
+                ax=ax)
+    ax.set_ylim(0., 1.)
+    ax.set_ylabel(f'% Runs : Max Grid Score > {grid_score_d90_threshold} | Pos Err < {low_pos_decoding_err_threshold}')
+    ax.set_xlabel('Group')
+
+    plt.savefig(os.path.join(plot_dir,
+                             f'percent_have_grid_cells_vs_run_group_given_low_pos_decoding_err.pdf'),
+                bbox_inches='tight',
+                dpi=300)
+    # plt.show()
+    plt.close()
+
+
+def plot_percent_low_decoding_err_vs_run_group(
+        runs_performance_df: pd.DataFrame,
+        plot_dir: str,
+        low_pos_decoding_err_threshold: float = 5.):
+    runs_performance_df[f'pos_decoding_err_below_{low_pos_decoding_err_threshold}'] = \
+        runs_performance_df['pos_decoding_err'] < low_pos_decoding_err_threshold
+
+    sns.barplot(x="run_group",
+                y=f'pos_decoding_err_below_{low_pos_decoding_err_threshold}',
+                data=runs_performance_df)
+    plt.xlabel('Group')
+    plt.ylabel(f'% Runs : Pos Error < {low_pos_decoding_err_threshold} cm')
+    plt.ylim(0., 1.)
+    plt.savefig(os.path.join(plot_dir,
+                             f'percent_low_decoding_err_vs_run_group.pdf'),
+                bbox_inches='tight',
+                dpi=300)
+    # plt.show()
+    plt.close()
+
+
+def plot_pos_decoding_err_vs_run_group(
         runs_performance_df: pd.DataFrame,
         plot_dir: str):
     sns.barplot(x="run_group",
                 y="pos_decoding_err",
                 data=runs_performance_df)
-    plt.xlabel('Models')
-    plt.ylim(0., 1.)
-    plt.savefig(os.path.join(plot_dir,
-                             f'percent_pos_decoding_err_vs_run_group.pdf'),
-                bbox_inches='tight',
-                dpi=300)
-    # plt.show()
-    plt.close()
-
-
-def plot_percent_pos_decoding_err_below_threshold_vs_run_group(
-        runs_performance_df: pd.DataFrame,
-        plot_dir: str,
-        threshold: float = 5.):
-    runs_performance_df[f'pos_decoding_err_below_{threshold}'] = \
-        runs_performance_df['pos_decoding_err'] < threshold
-
-    sns.barplot(x="run_group",
-                y=f'pos_decoding_err_below_{threshold}',
-                data=runs_performance_df)
+    plt.ylabel('Pos Decoding Err (cm)')
     plt.xlabel('Group')
-    plt.ylabel(f'Pos Decode Error < {threshold}')
-    plt.ylim(0., 1.)
     plt.savefig(os.path.join(plot_dir,
-                             f'percent_pos_decoding_err_below_threshold_vs_run_group.pdf'),
+                             f'pos_decoding_err_vs_run_group.pdf'),
                 bbox_inches='tight',
                 dpi=300)
     # plt.show()
--- notebooks/00_broad_sweep_results/00_broad_sweep_results.py
+++ notebooks/00_broad_sweep_results/00_broad_sweep_results.py
@@ -2,17 +2,20 @@ import os
 
 from mec_hpc_investigations.models.analyze import compute_minima_performance_metrics_from_runs_histories, \
     download_wandb_project_runs_configs, download_wandb_project_runs_histories
-from mec_hpc_investigations.models.plot import plot_percent_pos_decoding_err_vs_run_group, \
-    plot_percent_pos_decoding_err_below_threshold_vs_run_group
+from mec_hpc_investigations.models.plot import *
 
 
+# Declare
 plot_dir = 'notebooks/00_broad_sweep_results/results/'
-os.makedirs(plot_dir, exist_ok=True)
-
+low_pos_decoding_err_threshold = 5.
+grid_score_d60_threshold = 1.0
+grid_score_d90_threshold = 1.4
 sweep_ids = [
     '5bpvzhfh',  # Position task loss
 ]
 
+
+os.makedirs(plot_dir, exist_ok=True)
 
 runs_configs_df = download_wandb_project_runs_configs(
     wandb_project_path='mec-hpc-investigations',
@@ -31,20 +34,31 @@ minima_performance_metrics = compute_minima_performance_metrics_from_runs_histor
     runs_histories_df=runs_histories_df,
 )
 
-
 runs_performance_df = runs_configs_df[[
     'run_id', 'run_group', 'place_field_loss', 'place_field_values', 'place_field_normalization']].merge(
         minima_performance_metrics,
         on='run_id',
         how='left')
 
-
-plot_percent_pos_decoding_err_vs_run_group(
+plot_pos_decoding_err_vs_run_group(
     runs_performance_df=runs_performance_df,
     plot_dir=plot_dir)
 
-plot_percent_pos_decoding_err_below_threshold_vs_run_group(
+plot_percent_low_decoding_err_vs_run_group(
+    runs_performance_df=runs_performance_df,
+    plot_dir=plot_dir,
+    low_pos_decoding_err_threshold=low_pos_decoding_err_threshold)
+
+plot_pos_decoding_err_vs_max_grid_score_by_run_group(
     runs_performance_df=runs_performance_df,
     plot_dir=plot_dir)
+
+plot_percent_have_grid_cells_vs_run_group_given_low_pos_decoding_err(
+    runs_performance_df=runs_performance_df,
+    plot_dir=plot_dir,
+    low_pos_decoding_err_threshold=low_pos_decoding_err_threshold,
+    grid_score_d60_threshold=grid_score_d60_threshold,
+    grid_score_d90_threshold=grid_score_d90_threshold,
+)
 
 print(1)
